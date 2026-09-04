31f7e2d53cb1eea2c324580069583a5e582a2249
Global Scripts Development <rob4ikonly@gmail.com>
2026-09-04 10:50:15 +0500

    Create Premium_Data.lua

---
+++ Games/Premium_Data.lua
@@ -0,0 +1,17 @@
+-- // GLOBAL SCRIPTS: PREMIUM DATABASE \\ --
+-- // Hosted on GitHub \\ --
+
+local PremiumDatabase = {
+    -- This is the master secret. The game scripts will check against this.
+    -- Change this periodically for maximum security.
+    MasterSecret = "GlobalScripts_Secure_Test",
+    
+    -- List of authorized Premium users by their Roblox UserId
+    Players = {
+        [80349677404572] = { Status = true, Role = "Owner" }, -- Replace with your actual UserId
+        [123456789] = { Status = true, Role = "Premium User" },
+        [987654321] = { Status = true, Role = "Tester" }
+    }
+}
+
+return PremiumDatabase
